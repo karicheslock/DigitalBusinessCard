61de49df9c66ce31a1929caa46b00275a57d2a50
Kari Cheslock <karisd5274@gmail.com>
2022-04-02 16:28:22 -0700

    Contact updates

--- src/Intro.js
+++ src/Intro.js
@@ -6,7 +6,7 @@ export default function Intro() {
             <img className="my-photo" src="./images/kari_small.png" alt='kari cheslock' />
             <h2>Kari Cheslock</h2>
             <h3>Web Developer</h3>
-            <small><a className="personal-link" href={`www.karicheslock.com`}>www.karicheslock.com</a></small>
+            <small><a className="personal-link" href={`https://www.karicheslock.com/`}>www.karicheslock.com</a></small>
             <small><a className="personal-link" href={`https://www.github.com/karicheslock`}>https://www.github.com/karicheslock</a></small>
             <div className="intro-buttons">
                 <button className="email-button"><a className="intro-link" href={`mailto:karicheslock1@gmail.com`}>📧 Email</a></button>
